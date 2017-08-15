cd1d6fdadc8dc868d0a96af473b6046806250ba9
Ryan Moore <moorer@udel.edu>
2017-08-15 00:20:47 -0400

    Bump version

--- lsa.rb
+++ lsa.rb
@@ -169,7 +169,7 @@ module Aai
 end
 
 module Lsa
-  PIPELINE_VERSION = "0.7.0-alpha"
+  PIPELINE_VERSION = "0.7.0"
   COPYRIGHT = "2017 Ryan Moore"
   CONTACT   = "moorer@udel.edu"
   WEBSITE   = "https://github.com/mooreryan/lsa_for_genomes"
